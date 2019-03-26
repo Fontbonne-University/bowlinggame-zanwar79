6f6bd8476e97b05915f5df48b9f811619bac2839
FontbonneCloud <zanwar@fontbonne.edu>
2019-03-26 15:21:19 -0500

    oneStrike red phase

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -8,14 +8,61 @@ import org.junit.Test;
 
 public class BowlingTest {
 
+	private Game g;
+
 	@Before
 	public void setUp() throws Exception {
+		g = new Game();
+	}
 
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);  //spare
+	}
+
+	
+	private void rollMany(int n, int pins) {
+		for(int i=0; i< n; i++)
+			g.roll(pins);
 	}
 
 	@Test
-	public void canCreateGame()
+	public void canRoll()
 	{
-		Game g = new Game();
+		Game g= new Game();
+		g.roll(0);
+	}
+	
+	@Test
+	public void GutterGame()
+	{
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+
+	
+	@Test
+	public void allOnes()
+	{
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+	
+	@Test
+	public void oneSpare()
+	{
+		rollSpare();
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
+	}
+
+	@Test
+	public void oneStrike()
+	{
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
 	}
 }
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -2,5 +2,34 @@ package BowlingGame;
 
 public class Game {
 
+	int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+	
+	public void roll(int pins) {
+		// TODO Auto-generated method stub
+		rolls[currentRoll++] = pins;
+
+		
+	}
+
+	public int score() {
+		// TODO Auto-generated method stub
+		int firstInFrame=0;
+		for(int frame=0; frame< 10; frame++)
+		{
+			if(rolls[firstInFrame] + rolls[firstInFrame+1] == 10) //spare
+			{
+				score +=10 + rolls[firstInFrame+2];
+				firstInFrame+=2;		
+			}
+			else {
+				score += rolls[firstInFrame] + rolls[firstInFrame+1];
+				firstInFrame+=2;
+			}
+		}
+		return score;
+	}
+
 
 }
